3ba3e10b33da9d839539918865a8de03536171f0
ragilsatria <satriaragil97@gmail.com>
2020-08-25 21:20:55 +0700

    register login done

--- client/kazoom-apps/src/pages/Register.js
+++ client/kazoom-apps/src/pages/Register.js
@@ -1,28 +1,79 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
-export default () => {
+export const GET_USER = gql`
+    query {
+        users {
+            _id
+            username
+            password
+        }
+    }
+`;
+
+const ADD_USER = gql`
+    mutation($newUser: UserInput) {
+        addUser(newUser: $newUser) {
+            username
+            password
+        }
+    }
+`;
+
+export default (props) => {
+    const [addUser] = useMutation(ADD_USER, {
+        refetchQueries: [
+            {
+                query: GET_USER,
+            },
+        ],
+    });
+
+    const [userInput, setUserInput] = useState({
+        username: "",
+        password: "",
+    });
+
+    const onChange = (e) => {
+        let { name, value } = e.target;
+        const newUserInput = { ...userInput, [name]: value };
+        setUserInput(newUserInput);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addUser({
+            variables: {
+                newUser: userInput,
+            },
+        });
+        // console.log(userInput);
+        props.history.push("/login");
+    };
+
     return (
         <div style={{ padding: "10px", width: "500px", height: "300px" }}>
-            <form>
-                <div class="form-group">
-                    <label for="usernameRegister">Username</label>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
                     <input
+                        onChange={onChange}
                         type="text"
-                        class="form-control"
-                        name="usernameRegister"
+                        className="form-control"
+                        name="username"
                         aria-describedby="emailHelp"
                     />
                 </div>
-                <div class="form-group">
-                    <label for="passwordRegister">Password</label>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
                     <input
+                        onChange={onChange}
                         type="password"
-                        class="form-control"
-                        name="passwordRegister"
+                        className="form-control"
+                        name="password"
                     />
                 </div>
-                <button type="submit" class="btn btn-primary">
+                <button type="submit" className="btn btn-primary">
                     Register
                 </button>
             </form>
